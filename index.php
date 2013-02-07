f84d2cc2eb1ed2291b9a1f9c80d3cfa5bf8deb74
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-07 11:57:19 +0100

    refactoring

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
 <?php include '/includes/html_head.html'; ?>
 
-    <div id="login" data-role="page">
-        
+<div id="login" data-role="page">
+    <div data-role="header"></div>
     <div data-role="content">
         <div data-role="field-contain">
-        <form data-ajax="false" class="ui-body ui-body-c ui-corner-all" >
+            <form data-ajax="false" class="ui-body ui-body-c ui-corner-all">
                 <label for="customerName">Customer Name:</label>
-                <input type="text" name="name" id="customerName" value="benjamin" required />
-               
+                <input type="text" name="name" id="customerName" value="benjamin" required/>
+
                 <label for="customerPassword">Password:</label>
-                <input type="password" name="password" id="customerPassword"  required />
+                <input type="password" name="password" id="customerPassword" value="123" required/>
 
                 <label for="credentialsSubmit"></label>
                 <button type="button" name="submit" id="credentialsSubmit" value="Submit">Submit</button>
-        </form>            
-        <h2 class='loginErrMsg'>Fehler aufgetreten</h2>
-        </div><!-- Login-form-->
+            </form>
+            <h2 class='loginErrMsg'>Fehler aufgetreten</h2>
+        </div>
+        <!-- Login-form-->
 
-        </div><!--field-contain-->
+    </div>
+    <!--field-contain-->
 
-    </div><!--content-->
-    </div><!-- /login -->
+</div><!--content-->
+</div><!-- /login -->
 
 <?php include '/includes/footer.html'; ?>
