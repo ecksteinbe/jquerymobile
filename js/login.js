cc7d263acb56f43202b1a3b80699eed3c179e3e1
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-07 11:59:40 +0100

    refactoring, sessionstorage

--- js/login.js
+++ js/login.js
@@ -1,46 +1,44 @@
-$(document).ready(function() {
-	$('.loginErrMsg').hide();
-	$('#credentialsSubmit').click(function(){
-		var userName = $('#customerName').val();
-		var userPassword = $('#customerPassword').val();
-		console.log('Input Username: ' + userName + ' Input Password: ' + userPassword);
-    $.mobile.showPageLoadingMsg();
-		$.ajax({
-        url: rootUrl+'/JavaBackend/rest/auth/login', 
-  			dataType:'json',
-  			type:'post',
-  			data:{
-  				'username':userName,
-  				'password':userPassword
-  			},
-        xhrFields: {
-          withCredentials:true
-        },
-        crossDomain:true,
-  			success: function (data, textStatus, jqXHR){
-  				var respData = data.bodyData;
-  				var success = data.success;
+$ ( document ).ready ( function () {
+    $ ( '.loginErrMsg' ).hide ();
+    $ ( '#credentialsSubmit' ).click ( function () {
+        var userName = $ ( '#customerName' ).val ();
+        var userPassword = $ ( '#customerPassword' ).val ();
+        console.log ( 'Input Username: ' + userName + ' Input Password: ' + userPassword );
+        $.mobile.showPageLoadingMsg ();
+        $.ajax ( {
+            url: rootUrl + '/JavaBackend/rest/auth/login',
+            dataType: 'json',
+            type: 'post',
+            data: {
+                'username': userName,
+                'password': userPassword
+            },
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            success: function ( data, textStatus, jqXHR ) {
+                var respData = data.bodyData;
+                var success = data.success;
+                sessionStorage.setItem("allowedBankAccountId", respData.allowedBankAccountId);
+                if ( success ) {
+                    // hat funktioniert
+                    $.mobile.changePage ( 'home.php', { transition: "slideup"} );
+                    $ ( '#home' ).ready ( function () {
+                        alert ( 'Login erfolgreich' );
+                        $.mobile.hidePageLoadingMsg ();
+                    } );
+                } else {
+                    // hat nicht funktioniert
+                    $ ( 'h2' ).show ();
 
-  				if(success){
-  					// hat funktioniert
-  					$.mobile.changePage('home.php', { transition: "slideup"});
-            $('#home').ready(function(){
-              alert('Login erfolgreich');
-              $.mobile.hidePageLoadingMsg();
-            });
-  				} else {
-  					// hat nicht funktioniert
-  					$('h2').show();  
+                    showErrorMsg ();
 
-  					showErrorMsg();
-
-  				}		
-  			},
-  			error: function(jqXHR, textStatus, errorThrown){
-          showErrorXMLHttpRequest(jqXHR, textStatus, errorThrown);
-  			}
-
-		});
-	});
-});
-
+                }
+            },
+            error: function ( jqXHR, textStatus, errorThrown ) {
+                showErrorXMLHttpRequest ( jqXHR, textStatus, errorThrown );
+            }
+        } );
+    } );
+} );
