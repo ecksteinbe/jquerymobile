92f64a524a9c2eab59b37d440f704d593a43c8d6
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-07 11:46:38 +0100

    refactoring and variable AllowedBankAccountId

--- categoryPage.php
+++ categoryPage.php
@@ -1,58 +1,72 @@
 <?php include '/includes/html_head.html'; ?>
     
     <div id="categoryPage" data-role="page">
-        <?php include '/includes/header.html'; ?>
-    <div data-role="content">
-    	<!--Suchfeld --> 
-    	<!-- <form class="ui-listview-filter ui-bar-c" role="search">
-    		<label for="categorysearch"> Kategorien durchsuchen: </label>
-    		<input placeholder="Kategorien durchsuchen..." data-type="search" id="categorysearch" />
-    	</form> -->
-    	<!-- Liste mit Kategorien --> 
-    	<ul data-role="listview" data-filter="true" id="categoryView">
-    		<div data-role="collapsible-set" id="categoryViewListView" ></div> 
-    	</ul>
-    	<!-- Bearbeitung der Kategorien -->
-    	<label for="newCategory" class="description">Neue Kategorie hinzuf&uuml;gen:</label>
-    	<div data-role="fieldcontain" ><input type="text" id="newCategory" /></div>
-    	<div data-role="controlgroup" data-type="horizontal" data-mini="true" class="ui-corner-all ui-controlgroup ui-controlgroup-horizontal ui-mini"><div class="ui-controlgroup-controls">
-			<div data-role="button" id="newCatButton" data-icon="plus" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" class="ui-btn ui-btn-up-c ui-btn-icon-left ui-corner-left"><span class="ui-btn-inner ui-corner-left"><span class="ui-btn-text">Hinzuf&uuml;gen</span><span class="ui-icon ui-icon-plus ui-icon-shadow">&nbsp;</span></span></div>
-			<a href="index.html" data-role="button" id="delCatButton" data-icon="delete" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" class="ui-btn ui-btn-icon-left ui-corner-right ui-controlgroup-last ui-btn-up-c"><span class="ui-btn-inner ui-corner-right ui-controlgroup-last"><span class="ui-btn-text">Entfernen</span><span class="ui-icon ui-icon-delete ui-icon-shadow">&nbsp;</span></span></a>
-		</div>
-    </div><!--content-->
-	<script type="text/javascript">
-		$('#categoryPage').on('pageshow',function(){
-	        $.mobile.showPageLoadingMsg();
-	        $.ajax({
-	        	url: rootUrl+'/JavaBackend/rest/secure/bankaccount/1/categories', 
-	          	dataType:'json',
-	            xhrFields: {
-	                withCredentials:true
-	            },
-	            crossDomain:true,
-	            success: function (data, textStatus, jqXHR){
-	                var respData = data.bodyData;
-	                console.log(respData);
-	                //Pruefen ob success true ist
-	                if(data.success){
-	                    $.mobile.hidePageLoadingMsg();
-	                    categoryHandler(respData);
-	                }
-	                else{
-	                    categoryError(data.errorMsg);
-	                }
-	                console.log(respData[1].name); 
-	            },
-                error: function(jqXHR, textStatus, errorThrown){
-                    console.log('Error: ');
-                    transactionError('Fehler beim Request: ' + textStatus);
-                    ajaxError(function() {
-                         $(this).text( "Triggered ajaxError handler." );
-                    });
-                }	                   
-	            });  
-	  	});
-	  
-    </script> 
-    </div><!--/category-->
+		<?php include '/includes/header.html'; ?>
+        <div data-role="content">
+            <!--Suchfeld -->
+            <!-- <form class="ui-listview-filter ui-bar-c" role="search">
+	<label for="categorysearch"> Kategorien durchsuchen: </label>
+	<input placeholder="Kategorien durchsuchen..." data-type="search" id="categorysearch" />
+</form> -->
+            <!-- Liste mit Kategorien -->
+            <ul data-role="listview" data-filter="true" id="categoryView">
+                <div data-role="collapsible-set" id="categoryViewListView"></div>
+            </ul>
+            <!-- Bearbeitung der Kategorien -->
+            <label for="newCategory" class="description">Neue Kategorie hinzuf&uuml;gen:</label>
+
+            <div data-role="fieldcontain"><input type="text" id="newCategory"/></div>
+            <div data-role="controlgroup" data-type="horizontal" data-mini="true"
+                 class="ui-corner-all ui-controlgroup ui-controlgroup-horizontal ui-mini">
+                <div class="ui-controlgroup-controls">
+                    <div data-role="button" id="newCatButton" data-icon="plus" data-corners="true" data-shadow="true"
+                         data-iconshadow="true" data-wrapperels="span" data-theme="c"
+                         class="ui-btn ui-btn-up-c ui-btn-icon-left ui-corner-left"><span
+                            class="ui-btn-inner ui-corner-left"><span class="ui-btn-text">Hinzuf&uuml;gen</span><span
+                            class="ui-icon ui-icon-plus ui-icon-shadow">&nbsp;</span></span></div>
+                    <a href="index.html" data-role="button" id="delCatButton" data-icon="delete" data-corners="true"
+                       data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c"
+                       class="ui-btn ui-btn-icon-left ui-corner-right ui-controlgroup-last ui-btn-up-c"><span
+                            class="ui-btn-inner ui-corner-right ui-controlgroup-last"><span class="ui-btn-text">
+						Entfernen</span><span
+                            class="ui-icon ui-icon-delete ui-icon-shadow">&nbsp;</span></span></a>
+                </div>
+            </div>
+            <!--content-->
+            <script type="text/javascript">
+                $ ( '#categoryPage' ).on ( 'pageshow', function () {
+                    console.log ( 'CategoryPage wurde geladen.' );
+                    $.mobile.showPageLoadingMsg ();
+                    $.ajax ( {
+                        url: rootUrl + '/JavaBackend/rest/secure/bankaccount/'+sessionStorage.getItem('allowedBankAccountId')+'/categories',
+                        dataType: 'json',
+                        xhrFields: {
+                            withCredentials: true
+                        },
+                        crossDomain: true,
+                        success: function ( data, textStatus, jqXHR ) {
+                            var respData = data.bodyData;
+                            //Pruefen ob success true ist
+                            if ( data.success ) {
+                                $.mobile.hidePageLoadingMsg ();
+                                categoryHandler ( respData );
+                            }
+                            else {
+                                categoryError ( data.errorMsg );
+                            }
+                            //console.log ( 'Beispieldaten aus bodydata: ' + respData[1].name );
+                        },
+                        error: function ( jqXHR, textStatus, errorThrown ) {
+                            console.log ( 'Error: ' );
+                            categoryError ( 'Fehler beim Request: ' + textStatus );
+                            ajaxError ( function () {
+                                $ ( this ).text ( "Triggered ajaxError handler." );
+                            } );
+                        }
+                    } );
+                } );
+
+            </script>
+        </div>
+        <!--/category-->
 <?php include '/includes/footer.html'; ?>
