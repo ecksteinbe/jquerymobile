a61b69d4f8d06a0e1145481a00d080b6129ae977
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-07 11:57:51 +0100

    refactoring

--- js/filialfinder.js
+++ js/filialfinder.js
@@ -1,34 +1,32 @@
-$(document).ready(function(){
-$('.loginErrMsg').hide();
-$('#filialfinder').click(function(){
-	$.ajax({
-	  			url: rootUrl+'/JavaBackend/rest/secure/places?location=48.13661,11.57709', 
-	  			dataType:'json',
-	  			xhrFields: {
-	     			withCredentials:true
-	  			},
-	  			crossDomain:true,
-	  			<!--
-	  			success: function (data, textStatus, jqXHR){
-	  				var respData = data.bodyData;
-	  				var success = data.success;
+$ ( document ).ready ( function () {
+    $ ( '.loginErrMsg' ).hide ();
+    $ ( '#filialfinder' ).click ( function () {
+        $.ajax ( {
+            url: rootUrl + '/JavaBackend/rest/secure/places?location=48.13661,11.57709',
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
 
-	  				if(success){
-	  					// hat funktioniert
-	  					$.mobile.changePage("index.html#home");
-	  				} else {
-	  					// hat nicht funktioniert
-	  					//$('h2').show();
-	  					showErrorMsg();
-	  				}
+            success: function (data, textStatus, jqXHR){
+                var respData = data.bodyData;
+                var success = data.success;
 
-	  				
-	  			},
-	  			error: function(jqXHR, textStatus, errorThrown){
-	  			}
+                if(success){
+                // hat funktioniert
+                $.mobile.changePage("index.html#home");
+                } else {
+                // hat nicht funktioniert
+                //$('h2').show();
+                showErrorMsg ();
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+            }
 
-			});	
-	});
+        });
+    });
 });
 
 
