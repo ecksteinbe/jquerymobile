cc38e4fac3bb43beeec8c0f094023cea9eb61449
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-07 11:55:31 +0100

    refactoring

--- home.php
+++ home.php
@@ -1,10 +1,11 @@
 <?php include 'includes/html_head.html'; ?>
 
-    <div id="home" data-role="page">
-        <?php include 'includes/header.html'; ?>   
-        <div data-role="content">
-            <div id="showHomeText" align="center"> Mobile Banking mit JQuery Mobile - das ist Spass!</div>
-        </div><!--content-->
-    </div><!-- /home -->
+<div id="home" data-role="page">
+	<?php include 'includes/header.html'; ?>
+    <div data-role="content">
+        <div id="showHomeText" align="center"> Mobile Banking mit JQuery Mobile - das ist Spass!</div>
+    </div>
+    <!--content-->
+</div><!-- /home -->
 
 <?php include 'includes/footer.html'; ?>
