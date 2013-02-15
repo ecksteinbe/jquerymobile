db22a74dec0779fa3d294c0f1b79e4d949ae0eaa
Benjamin Eckstein <benjamin.eckstein@nttdata.com>
2013-02-15 14:50:17 +0100

    added function to read geolocation

--- filialfinder.php
+++ filialfinder.php
@@ -14,28 +14,34 @@
     </div>
     <!--content-->
     <script type="text/javascript">
-        $ ( '#filialfinder' ).on ( 'pageshow', function () {
-            $.mobile.showPageLoadingMsg ();
-            $.ajax ( {
-                url: rootUrl + '/JavaBackend/rest/secure/places?location=48.13661,11.57709',
-                xhrFields: {
-                    withCredentials: true
-                },
-                crossDomain: true,
-                success: function ( data, textStatus, jqXHR ) {
-                    $.mobile.hidePageLoadingMsg ();
-                    var respData = data.results;
-                    filialHandler ( respData );
-                    console.log ( respData );
+       
+        navigator.geolocation.getCurrentPosition(function(position){
+            
+            var lati = position.coords.latitude;
+            var longi = position.coords.longitude;
 
-                    console.log ( respData[0].name );
+            alert(lati + ', ' + longi);
 
+            $ ( '#filialfinder' ).on ( 'pageshow', function () {
+                $.mobile.showPageLoadingMsg ();
+                $.ajax ( {
+                    url: rootUrl + '/JavaBackend/rest/secure/places?location=' + lati + ',' + longi,
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    crossDomain: true,
+                    success: function ( data, textStatus, jqXHR ) {
+                        $.mobile.hidePageLoadingMsg ();
+                        var respData = data.results;
+                        filialHandler ( respData );
+                        console.log ( respData );
+                        console.log ( respData[0].name );
+                    },
+                    error: function ( jqXHR, textStatus, errorThrown ) {
+                        console.log ( 'Error: ' );
+                    }
 
-                },
-                error: function ( jqXHR, textStatus, errorThrown ) {
-                    console.log ( 'Error: ' );
-                }
-
+                } );
             } );
         } );
     </script>
